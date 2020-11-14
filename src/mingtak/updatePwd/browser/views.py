449b30fb69626ea80ded3abeb8c9a2662242a694
VS-BCART <andy@mingtak.com.tw>
2020-11-14 16:05:22 +0800

    update_password_self ok

--- src/mingtak/updatePwd/browser/views.py
+++ src/mingtak/updatePwd/browser/views.py
@@ -4,98 +4,87 @@ from mingtak.updatePwd import _
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone import api
-from zope.interface import Interface
-from plone.directives import form
-from zope import schema
-from z3c.form import button
-from Products.statusmessages.interfaces import IStatusMessage
+#from zope.interface import Interface
+#from plone.directives import form
+#from zope import schema
+#from z3c.form import button
+#from Products.statusmessages.interfaces import IStatusMessage
 
 
-class IUpdatePasswordSelf(form.Schema):
+class UpdatePasswordSelf(BrowserView):
+    """ 更新自己密碼 """
 
-    newPassword = schema.TextLine(
-        title=_(u"New Password"),
-    )
+    template = ViewPageTemplateFile('templates/update_password_self.pt')
+
+    def __call__(self):
+
+        request = self.request
+
+        if 'submit' in request.form:
+            result = self.handleApply()
+            if result:
+                api.portal.show_message(message=_('密碼更新成功'), request=request)
+                return self.template()
+            else:
+                api.portal.show_message(message=_('密碼更新失敗，請重新輸入'), request=request, type='error')
+                return self.template()
+        return self.template()
 
 
-class UpdatePasswordSelf(form.SchemaForm):
-
-    schema = IUpdatePasswordSelf
-    ignoreContext = True
-
-    label = _(u"Update Password")
-    description = _(u"Please new password, update it.")
-
-
-    @button.buttonAndHandler(_(u'Setup'))
-    def handleApply(self, action):
+    def handleApply(self):
         request = self.request
         response = request.response
         portal = api.portal.get()
-        data, errors = self.extractData()
-        if errors:
-            self.status = self.formErrorsMessage
+
+        if api.user.is_anonymous():
+            response.redirect(portal.absolute_url())
             return
 
-        newPassword = self.request.form['form.widgets.newPassword']
+        newPassword = self.request.form['newPassword']
+        newPassword2 = self.request.form['newPassword2']
+
+        import pdb; pdb.set_trace()
+        if newPassword != newPassword2 or len(newPassword) <= 5:
+            return False
+
         user = api.user.get_current()
         accountid = user.id
         if user:
             user.setSecurityProfile(password=newPassword)
-            message = _(u"Already update password!")
-            mType = 'info'
-        else:
-            message = _(u"User not found!")
-            mType = 'warning'
+            return True
+        return False
 
-        response.redirect(portal.absolute_url())
-        api.portal.show_message(message=message, request=request, type=mType)
-        return
 
-    @button.buttonAndHandler(_(u"Cancel"))
-    def handleCancel(self, action):
+class UpdatePassword(BrowserView):
 
+    template = ViewPageTemplateFile('templates/update_password.pt')
+
+    def __call__(self):
+
+        request = self.request
+
+        if 'submit' in request.form:
+            result = self.handleApply()
+            if result:
+                api.portal.show_message(message=_('密碼更新成功'), request=request)
+                return self.template()
+            else:
+                api.portal.show_message(message=_('密碼更新失敗，請重新輸入'), request=request, type='error')
+                return self.template()
+        return self.template()
+
+
+    def handleApply(self):
         request = self.request
         response = request.response
         portal = api.portal.get()
-        response.redirect(portal.absolute_url())
-        message = _(u"Cancel this action!")
-        api.portal.show_message(message=message, request=request, type='info')
-        return
 
-
-class IUpdatePassword(form.Schema):
-
-    accountId = schema.TextLine(
-        title=_(u"Account Id"),
-    )
-
-    newPassword = schema.TextLine(
-        title=_(u"New Password"),
-    )
-
-
-class UpdatePassword(form.SchemaForm):
-
-    schema = IUpdatePassword
-    ignoreContext = True
-
-    label = _(u"Update Password")
-    description = _(u"Please input account id and password, update it.")
-
-
-    @button.buttonAndHandler(_(u'Setup'))
-    def handleApply(self, action):
-        request = self.request
-        response = request.response
-        portal = api.portal.get()
-        data, errors = self.extractData()
-        if errors:
-            self.status = self.formErrorsMessage
+        if api.user.is_anonymous():
+            response.redirect(portal.absolute_url())
             return
 
-        accountId = self.request.form['form.widgets.accountId']
-        newPassword = self.request.form['form.widgets.newPassword']
+        accountId = self.request.form['accountId']
+        newPassword = self.request.form['newPassword']
         user = api.user.get(username=accountId)
         if user:
             user.setSecurityProfile(password=newPassword)
@@ -105,17 +94,6 @@ class UpdatePassword(form.SchemaForm):
             message = _(u"User not found!")
             mType = 'warning'
 
-        response.redirect('%s/@@update_password' % portal.absolute_url())
+        response.redirect('%s/%s' % (portal.absolute_url(), self.__name__))
         api.portal.show_message(message=message, request=request, type=mType)
-        return
-
-    @button.buttonAndHandler(_(u"Cancel"))
-    def handleCancel(self, action):
-
-        request = self.request
-        response = request.response
-        portal = api.portal.get()
-        response.redirect('%s/@@update_password' % portal.absolute_url())
-        message = _(u"Cancel this action!")
-        api.portal.show_message(message=message, request=request, type='info')
         return
